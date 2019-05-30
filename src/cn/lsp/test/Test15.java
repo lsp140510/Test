53e9ccb6e7f505bf344b473c85bf61c4bf24fd55
lsp140510 <lsp57263@163.com>
2019-05-30 23:07:36 +0800

    无

---
+++ src/cn/lsp/test/Test15.java
@@ -0,0 +1,27 @@
+package cn.lsp.test;
+
+import java.util.Scanner;
+
+public class Test15 {
+	public static void main(String args[]) {
+		Scanner cin = new Scanner(System.in);
+		int a, b, c;
+		while (cin.hasNextInt()) {
+			a = cin.nextInt();
+			b = cin.nextInt();
+			c = cin.nextInt();
+			// System.out.println(a);
+			// System.out.println(b);
+			// System.out.println(c);
+			if (b * b - 4 * a * c < 0) {
+				System.out.println(0);
+			} else if (b * b - 4 * a * c == 0) {
+				System.out.println(-b / (2 * a));
+			} else if (b * b - 4 * a * c > 0) {
+				System.out.format("%.4f %.4f",(((-b) - Math.sqrt(b * b - 4 * a * c) )/(2*a)),(((-b) + Math.sqrt(b * b - 4 * a * c) )/(2*a)) 
+						);
+			}
+		}
+
+	}
+}
